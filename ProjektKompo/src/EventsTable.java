49ddc46a013d76447d50e0a1e0673ffddddcd4fd
Ralink06 <rafal14kop@gmail.com>
2016-06-04 15:20:29 +0200

    EventsTabble napisane wszystko

---
+++ ProjektKompo/src/EventsTable.java
@@ -0,0 +1,23 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+
+public class EventsTable extends JTable{
+	
+	//String [][]rows;
+	//String []columns= new String[5];
+	
+	
+	
+	EventsTable()
+	{
+	//super();  tutaj w argumencie daj to co Ci wysallem
+		
+		
+		
+	}
+
+}
